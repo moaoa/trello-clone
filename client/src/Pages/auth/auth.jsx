46daf19c98adbb4aaff91d56d6e3ec29c6ab3d5b
moaad <moaadbn3@gmail.com>
2020-10-25 11:09:49 +0200

    connected backend with frontend in google auth proccess

--- client/src/Pages/auth/auth.jsx
+++ client/src/Pages/auth/auth.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './auth.css'
 import InputField from '../../components/InputField/InputField'
-import axios from 'axios'
 
 export default function InputAdornments() {
     return (
@@ -10,7 +9,9 @@ export default function InputAdornments() {
                 <h3>Sign Up</h3>
                     <InputField placeholder='email' type='email'/>
                     <InputField placeholder='password' type='password'/>
-                    <button onClick={() => axios.get('/auth/google') } className='btn'>sign up</button>
+                    <button onClick={() => {
+                        window.open('http://localhost:5000/auth/google', '_self')
+                    } } className='btn'>sign up</button>
             </div>
         </div>
     )
